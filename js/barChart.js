8da9c759cbd69b4745d5649ddc9cf2d6891ffe5f
Daffa Naufal Mumtaz Heryadi <149856104+Neuraly4@users.noreply.github.com>
2025-04-25 21:22:22 +0700

    Bar Plot

--- js/barChart.js
+++ js/barChart.js
@@ -1,20 +1,33 @@
-//ini draft
 const barTargetYear = 2023;
-const barTargetKomoditas = "Cabai Merah";
+const topKomoditas = ["Cabai Rawit", "Cabai Besar", "Tomat"];
+const targetProvinsi = ["JAWA TENGAH", "JAWA TIMUR", "JAWA BARAT", "SUMATERA UTARA", "SUMATERA BARAT"];
 
-const barData = produksiData.filter(
-  d => d.tahun === barTargetYear && d.komoditas === barTargetKomoditas
+// Filter data hanya untuk tahun dan komoditas serta provinsi yang relevan
+const barDataFiltered = produksiData.filter(
+  d => d.tahun === barTargetYear &&
+       topKomoditas.includes(d.komoditas) &&
+       targetProvinsi.includes(d.provinsi)
 );
 
-const barX = barData.map(d => d.provinsi);
-const barY = barData.map(d => d.produksi);
+// Kelompokkan data berdasarkan komoditas
+const groupedData = topKomoditas.map(komoditas => {
+  const dataKomoditas = targetProvinsi.map(prov => {
+    const record = barDataFiltered.find(d => d.komoditas === komoditas && d.provinsi === prov);
+    return record ? record.produksi : 0;
+  });
 
-Plotly.newPlot("barplot", [{
-  x: barX,
-  y: barY,
-  type: 'bar'
-}], {
-  title: `Produksi ${barTargetKomoditas} di Tiap Provinsi (${barTargetYear})`,
+  return {
+    x: targetProvinsi,
+    y: dataKomoditas,
+    name: komoditas,
+    type: 'bar'
+  };
+});
+
+// Plot grouped bar chart
+Plotly.newPlot("barplot", groupedData, {
+  title: `Produksi Tiga Komoditas Sayur Tertinggi di 5 Provinsi (${barTargetYear})`,
+  barmode: 'group',
   xaxis: { title: "Provinsi" },
   yaxis: { title: "Produksi (Ton)" }
 });
